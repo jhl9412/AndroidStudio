99a58294dc14adbe9125808db6dccd0704a5e6c0
jhl9412 <102011846+jhl9412@users.noreply.github.com>
2022-04-16 11:38:35 +0900

    Add files via upload

---
+++ main/java/com/example/exam03/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.exam03;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webView);
+
+        String url = "10.10.141.51:8080";
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+    }
+}
